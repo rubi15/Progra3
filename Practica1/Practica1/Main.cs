f5614dc9e280d7244544abc7d3ac186b82ada0f4
Rafael <rahp_27@hotmail.com>
2013-09-02 19:36:26 -0500

    Primer commit

---
+++ Practica1/Practica1/Main.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace Practica1
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			char s;
+
+			Console.WriteLine ("Haaay Rafa, Como estamos?\nb/m");
+			s = Console.ReadLine ();
+			if (s == b) {
+				Console.WriteLine ("Que chido");
+			}
+			if (s == m) {
+				Console.WriteLine("Que mal");
+			}
+			else {
+				Console.WriteLine("Ponga algo valido, no mame");
+			}
+		}
+	}
+}
